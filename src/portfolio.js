0aa51307c917c9bcf396cd487aa3fcb4efb6b431
mayheptad <wfoluwarichie@gmail.com>
2022-03-16 13:00:57 +0100

    change link to resume

--- src/portfolio.js
+++ src/portfolio.js
@@ -25,7 +25,7 @@ const greeting = {
   subTitle:
     "A software Developer, A passionate individual who always thrives to work on end to end products which develop sustainable, scalable social & technical systems to create impact.",
   resumeLink:
-    "https://drive.google.com/file/d/1P7EYl9OrftppJspf4NfhLinH1teTD9o9/view?usp=sharing",
+    "https://drive.google.com/file/d/19LIOXewQr5O9Spq0aoVz_4sKqLFr430R/view?usp=sharing",
   portfolio_repository: "https://github.com/Mayheptad/smart-vision-ai_frontend",
   githubProfile: "https://github.com/mayheptad",
 };
